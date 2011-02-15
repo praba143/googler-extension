947459b7bddab17750244906959599df6dd4ac79
julgoor <julgoor@bfb1f5b8-efab-78ff-7d45-b8f90c36182f>
2011-02-15 09:39:37 +0000

    Actualizacion

--- trunk/googler/configuration.js
+++ trunk/googler/configuration.js
@@ -3,13 +3,14 @@
 *                Configuration                *
 ***********************************************/
 
-search_engines = [];
+_searchEngines = [];
 delay = 1;
+//   width: $(page.width);
 
 function loadConfiguration (configuration) {
     // Load Google Info
-    search_engines.push ( loadGoogleInfo() );
-    search_engines.push ( loadBingInfo() );
+    _searchEngines.push ( loadGoogleInfo() );
+    _searchEngines.push ( loadBingInfo() );
 
 }
 
@@ -26,9 +27,10 @@ function loadGoogleInfo () {
     info["Start"] = "&start=";
     info["Operators"] = ["allinanchor", "allintext", "allintitle", "allinurl", "cache", "define", "filetype", "id", "inanchor", "info", "intext", "intitle", "inurl", "link", "phonebook", "related", "site"];
 
-    info["Regexp"] = '<div class=vsc sig=[^>]*?><span class=tl><h3 class="r"><a href="[^"]*?" class=l onmousedown="[^"]*?">.*?</a></h3><button class=ws title=""></button><button class=vspib></button>.*?</span><div class="s">.*?<span class=f><cite>.*?</cite> - <span class=gl><a href="[^"]*?" onmousedown="[^"]*?">';
+    //info["Regexp"] = '<div class=vsc sig=[^>]*?><span class=tl><h3 class="r"><a href="[^"]*?" class=l onmousedown="[^"]*?">.*?</a></h3><button class=ws title=""></button><button class=vspib></button>.*?</span><div class="s">.*?<span class=f><cite>.*?</cite> - <span class=gl><a href="[^"]*?" onmousedown="[^"]*?">';
+    info["Regexp"] = 'class=vsc.*?</cite>';
 
-    info["RegexpURL"] = '<a href="[^"]*?" class=l';
+    //info["RegexpURL"] = '<a href="[^"]*?" class=l';
     info["ExtractURL"] = function (data) {
         var pattern = new RegExp ('<a href="[^"]*?" class=l', "gi");
         var res = data.match(pattern);
@@ -38,7 +40,7 @@ function loadGoogleInfo () {
             return "";
     };
 
-    info["RegexpTitle"] = '<a href="[^"]*" class=l onmousedown="[^"]*">.*?</a></h3>';
+    //info["RegexpTitle"] = '<a href="[^"]*" class=l onmousedown="[^"]*">.*?</a></h3>';
     info["ExtractTitle"] = function (data) {
         var pattern = new RegExp ('onmousedown="[^"]*">.*?</a></h3>', "gi");
         var res = data.match(pattern);
@@ -48,8 +50,7 @@ function loadGoogleInfo () {
             return "";
     };
 
-    //info["RegexpDesc"] = 'class="s">.*?<br><span';
-    info["RegexpDesc"] = '</span><div class="s">.*?<br><span';
+    //info["RegexpDesc"] = '</span><div class="s">.*?<br><span';
     info["ExtractDesc"] = function (data) {
         var pattern = new RegExp ('</span><div class="s">.*?<br><span', "gi");
         var res = data.match(pattern);
@@ -59,7 +60,6 @@ function loadGoogleInfo () {
             return "";
     }
 
-    info["DescLimits"] = [10, 9];
     //info[""] = "";
 
     return info;
@@ -78,9 +78,10 @@ function loadBingInfo () {
     info["Start"] = "&first=";
     info["Operators"] = ["contains", "filetype", "inanchor", "inbody", "intitle", "ip", "language", "location", "prefer", "site", "feed", "hasfeed"];
 
-    info["Regexp"] = '<div class="sa_cc"><div class="sb_tlst"><h3><a href="[^"]*?" onmousedown="[^"]*?">.*?</a></h3></div>.*?<div class="sb_meta"><cite>';
-
-    info["RegexpURL"] = '<h3><a href="[^"]*?" onmousedown';
+    //info["Regexp"] = '<div class="sa_cc"><div class="sb_tlst"><h3><a href="[^"]*?" onmousedown="[^"]*?">.*?</a></h3></div>.*?<div class="sb_meta"><cite>';
+    info["Regexp"] = '<div class="sa_cc"><div class="sb_tlst">.*?<cite>';
+    
+    //info["RegexpURL"] = '<h3><a href="[^"]*?" onmousedown';
     info["ExtractURL"] = function (data) {
         var pattern = new RegExp ('<h3><a href="[^"]*?" onmousedown', "gi");
         var res = data.match(pattern);
@@ -90,7 +91,7 @@ function loadBingInfo () {
             return "";
     };
 
-    info["RegexpTitle"] = 'onmousedown="[^"]*">.*?</a></h3>';
+    //info["RegexpTitle"] = 'onmousedown="[^"]*">.*?</a></h3>';
     info["ExtractTitle"] = function (data) {
         var pattern = new RegExp ('onmousedown="[^"]*">.*?</a></h3>', "gi");
         var res = data.match(pattern);
@@ -100,7 +101,7 @@ function loadBingInfo () {
             return "";
     };
 
-    info["RegexpDesc"] = '<p>.*?</p><div';
+    //info["RegexpDesc"] = '<p>.*?</p><div';
     info["ExtractDesc"] = function (data) {
         var pattern = new RegExp ('<p>.*?</p><div', "gi");
         var res = data.match(pattern);
@@ -110,9 +111,12 @@ function loadBingInfo () {
             return "";
     }
     
-    info["DescLimits"] = [7, 8];
     info["Help"] = "http://msdn.microsoft.com/en-us/library/ff795667.aspx";
     //info[""] = "";
+
+    info["Cookies"] = [];
+    info["Cookies"]["Need"] = "UPDATE";
+    info["Cookies"]["UPDATE"] = "";
 
     return info;
 }
@@ -136,40 +140,3 @@ function loadYahooInfo () {
     return info;
 }
 
-
-/*
- *        
-
-<searchEngineSignature>
-<searchEngine>
-	<Name>Bing</Name>
-	<Prefix>L3Jlc3VsdHMuYXNweD9xPQ==</Prefix>
-	<Server>http://search.msn.com</Server>
-	<Matchstring>V2ViIHJlc3VsdHNccy4qXHNvZlxzKC4qPyk8XC9zcGFuPg==</Matchstring>
-	<ResultsMatchstring>PGxpPjxoMz48YSBocmVmPSJbXiJdKj8i</ResultsMatchstring>
-	<NextMatchstring>Lio8YVxzK2NsYXNzPVwiblBcIiBocmVmPVwiKC4qPylcIj5OZXh0PFwvYT48XC9kaXY+</NextMatchstring>
-	<CacheMatchstring>PGxpXHNjbGFzcz1cImNhY2hlZFwiPjxiPiZcIzAxODM7PFwvYj5cczxhIGhyZWY9XCIoLio/KVwiPkNhY2hlZCBwYWdlPFwvYT4=</CacheMatchstring>
-	<Site>IHNpdGUlM0E=</Site>
-	<Ip>IGlwJTNB</Ip>
-	<Intitle>IGludGl0bGUlM0E=</Intitle>
-	<Inurl>IGludXJsJTNB</Inurl>
-	<Filetype>IGZpbGV0eXBlJTNB</Filetype>
-</searchEngine>
-
-
-<searchEngine>
-	<Name>Yahoo</Name>
-	<Prefix>L3NlYXJjaD9wPQ==</Prefix>
-	<Server>search.yahoo.com</Server>
-	<Matchstring>PHNwYW4gaWQ9XCJpbmZvdG90YWxcIj4oLio/KVs8L3NwYW4+XHN8XHNmcm9tXQ==</Matchstring>
-	<ResultsMatchstring>eXNjaHR0bFxzaHJlZj1cIiguKj8pXCJccz4=</ResultsMatchstring>
-	<NextMatchstring>PGEgaWQ9InBnLW5leHQiIGNsYXNzPSJwZyIgaHJlZj1cIiguKj8pXCI+TmV4dA==</NextMatchstring>
-	<CacheMatchstring>PGEgaHJlZj1cIiguKj8pXCI+Q2FjaGVkPFwvYT4=</CacheMatchstring>
-	<Site>IHNpdGUlM0E=</Site>
-	<Ip>IHNpdGUlM0E=</Ip>
-	<Intitle>IGludGl0bGUlM0E=</Intitle>
-	<Inurl>IGludXJsJTNB</Inurl>
-	<Filetype>JnZmPQ==</Filetype>
-</searchEngine>
-</searchEngineSignature>
-*/
